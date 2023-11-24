7a8b29405525cf491dbd5cc866e6364bca2c2b4e
kibet <nehmz254@gmail.com>
2023-11-24 20:59:40 +0300

    Adding files

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # Installs flask from pip.
 
 package { 'python3-pip':
-  ensure => installed,
+	ensure => installed,
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+	command => '/usr/bin/pip3 install flask==2.1.0',
+	path    => ['/usr/bin'],
+	unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+	require => Package['python3-pip'],
 }
